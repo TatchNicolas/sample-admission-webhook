95fb6e4a73280d71af10b648c667950e398140c6
TatchNicolas <TatchNicolas@users.noreply.github.com>
2019-12-11 18:20:06 +0900

    Return request body as message for debugging

--- istio-vsvc-host/main.py
+++ istio-vsvc-host/main.py
@@ -1,4 +1,7 @@
+import json
+
 from flask import Flask, jsonify, request
+
 
 app = Flask(__name__)
 
@@ -9,21 +12,13 @@ VALIDATION_ERROR_MSG = 'FOO collides with existing host settings'
 def validate():
     try:
         req = request.get_json()
-        env = req['request']['object']['metadata'].get('labels',{}).get('env')
-        # env ラベルが dev,stg,prd のいずれかであることをチェック
-        if env in ('dev', 'stg', 'prd'):
-            is_valid = True
-            message = VALIDATION_ERROR_MSG
-        else:
-            is_valid = False
-            message = VALIDATION_ERROR_MSG
         return jsonify({
               'apiVersion': 'admission.k8s.io/v1',
               'kind': 'AdmissionReview',
               'response': {
                 'uid': request.get_json()['request']['uid'],
-                'allowed': is_valid,
-                'status': {'message': message}
+                'allowed': True,
+                'status': {'message': json.dumps(req)}
                 }
         }), 200
     except (TypeError, KeyError):
