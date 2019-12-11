cb5b0125c1ca42589c6e445a0805b137b4e32a6d
TatchNicolas <TatchNicolas@users.noreply.github.com>
2019-12-11 19:32:15 +0900

    WIP

--- istio-vsvc-host/main.py
+++ istio-vsvc-host/main.py
@@ -1,6 +1,7 @@
 import json
 
 from flask import Flask, jsonify, request
+from kubernetes import client, config
 
 
 app = Flask(__name__)
@@ -8,10 +9,24 @@ app = Flask(__name__)
 VALIDATION_OK_MSG = 'Validation succeeded'
 VALIDATION_ERROR_MSG = 'FOO collides with existing host settings'
 
+# config.load_incluster_config()
+config.load_kube_config()
+conf=client.Configuration()
+api_client=client.ApiClient(configuration=conf)
+
 @app.route('/validate', methods=['POST'])
 def validate():
     try:
         req = request.get_json()
+        hosts = req['request']['object']['spec']['hosts']
+        print(hosts)
+        apiserver_resp = api_client.call_api(
+                '/apis/networking.istio.io/v1alpha3/virtualservices','GET',
+                auth_settings=['BearerToken'],
+                response_type='object',
+        )
+        existing_hosts = apiserver_resp[0]['items']
+        print(existing_hosts)
         return jsonify({
               'apiVersion': 'admission.k8s.io/v1',
               'kind': 'AdmissionReview',
